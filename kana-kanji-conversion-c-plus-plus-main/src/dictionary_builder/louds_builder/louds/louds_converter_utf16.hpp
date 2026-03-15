0baa0110718372fc8cd0d7c063289c16d3f77be7
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2026-03-16 04:41:50 +0900

    delete unnecessary files

--- kana-kanji-conversion-c-plus-plus-main/src/dictionary_builder/louds_builder/louds/louds_converter_utf16.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "prefix_tree/prefix_tree_utf16.hpp"
-#include "louds/louds_utf16_writer.hpp"
-
-class ConverterUtf16
-{
-public:
-    LOUDSUtf16 convert(const PrefixNodeUtf16 *rootNode) const;
-};
